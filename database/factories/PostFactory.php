cbfbffe8d998c8f4db7f69de0c349024e3c96d4c
Simon Gheux <simongheux@MacBook-Pro-de-Simon.local>
2018-05-22 14:31:11 +0200

    ajout du seeder pour les posts + ajout d'un utilisateur de base

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -4,6 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Post::class, function (Faker $faker) {
     return [
-        //
+        'titre' => $faker->sentence(rand(4,8)), 
+        'contenu' => $faker->paragraph(rand(5,10)),
     ];
 });
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        App\User::create([
+            "name" => "Simon Gheux",
+            "email" => "simongheux@gmail.com",
+            "password" => bcrypt('admin1234'),
+        ]);
+    }
+}
