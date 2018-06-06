f0d11c3e04aee77d9f6af727327a712b7ef152b8
simonghx <simongheux@gmail.com>
2018-06-06 16:25:08 +0200

    ajout du role_id dans le registerprovider + dans le register controller + genrate des dossiers listeners et events + ajout des events messages (voir doc)

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -16,6 +16,14 @@ class EventServiceProvider extends ServiceProvider
         'App\Events\Event' => [
             'App\Listeners\EventListener',
         ],
+
+        'Illuminate\Mail\Events\MessageSending' => [
+            'App\Listeners\LogSendingMessage',
+         ],
+
+        'Illuminate\Mail\Events\MessageSent' => [
+            'App\Listeners\LogSentMessage',
+        ],
     ];
 
     /**
